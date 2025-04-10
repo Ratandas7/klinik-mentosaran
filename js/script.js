e07fe31f07fdb04653524e07be6d631de12ae067
Ratandas7 <ratandas7720@gmail.com>
2025-04-10 20:41:07 +0600

    klinik-mentosaran

---
+++ js/script.js
@@ -0,0 +1,5 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navBar = document.querySelector(".menu_container");
+    navBar.classList.toggle("active");
+}
